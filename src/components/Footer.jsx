84aba2a249e56b6bc4d293608549940340ab9861
lukas <l.borkowski@madkom.pl>
2024-07-26 10:07:30 +0200

    adjusts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,6 +28,7 @@ export const Footer = () => {
                   d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
                   clip-rule="evenodd"
                 />
+                
               </svg>
               <span class="sr-only">Facebook page</span>
             </a>
