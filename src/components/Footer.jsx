ec579a47605c55db25caf7816c60d2ec26b6ac58
lukas <l.borkowski@madkom.pl>
2024-07-26 11:32:45 +0200

    adjusts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer class="bg-white dark:bg-gray-900">
+    <footer class=" dark:bg-gray-900">
       <div class="mx-auto w-full max-w-6xl p-4 py-6 lg:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
@@ -28,7 +28,7 @@ export const Footer = () => {
                   d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
                   clip-rule="evenodd"
                 />
-                
+
               </svg>
               <span class="sr-only">Facebook page</span>
             </a>
